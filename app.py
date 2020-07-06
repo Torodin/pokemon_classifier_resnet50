464d81068686912a448ad48c16406fbf65dd849c
Diego <diegogomezledo@gmail.com>
2020-07-07 00:57:11 +0200

    Añadido index

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import os
 from fastai.basic_train import load_learner
 from fastai.vision import open_image
 from flask_cors import CORS,cross_origin
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 CORS(app, support_credentials=True)
 
 learn = load_learner(path='./models', file='poke_calsf_rsn50.pkl')
@@ -21,5 +22,9 @@ def predict(img_file):
 def predict():
     return jsonify(predict_single(request.files['image']))
 
+@app.route('/')
+def render_page():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False, port=os.getenv('PORT',5000))
